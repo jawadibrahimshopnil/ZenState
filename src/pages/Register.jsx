00fef3f48f8a521ea55e00b4b658c2c33b9d7b55
jawadibrahimshopnil <jawadibrahimshopnil01@gmail.com>
2024-04-15 15:52:12 +0600

    added register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,35 @@
 const Register = () => {
     return (
-        <div></div>
+        <section className="grid place-items-center m-6">
+            <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-50 border">
+                <h1 className="text-2xl font-bold text-center">Login</h1>
+                <form noValidate="" action="" className="space-y-6">
+                    <div className="space-y-1">
+                        <label htmlFor="name" className="block dark:text-gray-600">Name</label>
+                        <input type="text" name="name" id="name" placeholder="Name" className="w-full px-4 py-3 border rounded-md border-gray-400" />
+                    </div>
+                    <div className="space-y-1">
+                        <label htmlFor="photoUrl" className="block dark:text-gray-600">Photo URL</label>
+                        <input type="text" name="photoUrl" id="photoUrl" placeholder="Photo URL" className="w-full px-4 py-3 border rounded-md border-gray-400" />
+                    </div>
+                    <div className="space-y-1">
+                        <label htmlFor="email" className="block dark:text-gray-600">Email</label>
+                        <input type="text" name="email" id="email" placeholder="Email" className="w-full px-4 py-3 border rounded-md border-gray-400" />
+                    </div>
+                    <div className="space-y-1">
+                        <label htmlFor="password" className="block dark:text-gray-600">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 border rounded-md border-gray-400" />
+                        <div className="flex justify-end text-sm dark:text-gray-600">
+                            <a rel="noopener noreferrer" href="#">Forgot Password?</a>
+                        </div>
+                    </div>
+                    <button className="block w-full p-3 text-center rounded-md dark:text-gray-50 dark:bg-violet-600">Register in</button>
+                </form>
+                <p className=" text-center sm:px-6 dark:text-gray-600">Already have an account?
+                    <a rel="noopener noreferrer" href="#" className="underline text-blue-600"> Login</a>
+                </p>
+            </div>
+        </section>
     );
 };
 

9f6f8dc443d10a4af813028ea3d2fc36bc877a54
jawadibrahimshopnil <jawadibrahimshopnil01@gmail.com>
2024-04-17 15:30:26 +0600

    added a new routes

---
+++ src/pages/FavoriteProp.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { FavPropertyContext, PropertyContext } from "./Root";
+import PropertyCard from "../components/PropertyCard";
+
+const FavoriteProp = () => {
+    const {favProps} = useContext(FavPropertyContext);
+    const properties = useContext(PropertyContext);
+
+    const selectedProperty = properties.filter(property=>favProps.includes(property.id));
+    console.log(selectedProperty, favProps);
+
+    return (
+        <div className="my-8 md:my-12">
+            <div>
+                <h1 className="text-center text-4xl font-bold mb-1 md:mb-6">Your Favorite</h1>
+                <p className="px-8 md:px-28 text-center mb-6 md:mb-12">Explore our curated collection of exquisite properties, meticulously selected to embody elegance, luxury, and unparalleled comfort. Begin your journey towards finding your dream home with ZenState today.</p>
+            </div>
+            <div id="estate" className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 px-6">
+                {
+                    selectedProperty.map((property,idx)=><PropertyCard key={idx} data={property}></PropertyCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default FavoriteProp;

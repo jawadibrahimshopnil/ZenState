08fb13bf4182e6f59e4cc074de9d62db50fa1db1
jawadibrahimshopnil <jawadibrahimshopnil01@gmail.com>
2024-04-18 17:14:08 +0600

    added readme file

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -23,6 +23,8 @@ const Slider = () => {
                 <SwiperSlide><img src='https://ap.rdcpix.com/2a0c0db6c3d3c6118a5da0ad553b8d81l-m3579874056rd-w1080_h768.webp' /></SwiperSlide>
                 <SwiperSlide><img src='https://ap.rdcpix.com/4e84fde6a88e5c7bf2eab75a3cb724bbl-m2939401519rd-w1080_h768.webp' /></SwiperSlide>
                 <SwiperSlide><img src='https://ap.rdcpix.com/c112dac8d9a2ee8b8132f13e3f312acfl-m412048566od-w1080_h768.webp' /></SwiperSlide>
+                <SwiperSlide><img src='https://ap.rdcpix.com/b04341e0b7596f0646e86d9565ace695l-m160559136rd-w1080_h768.webp' /></SwiperSlide>
+                <SwiperSlide><img src='https://ap.rdcpix.com/561b886673b31609e72e663df283d29al-m2830088036od-w1080_h768_x2.webp' /></SwiperSlide>
             </Swiper>
         </div>
     );
